365ad3175fc9f240be685b41ca02331bba5f638f
Nate <nathanbarlow23@gmail.com>
2020-11-10 18:33:56 -0700

    add mongo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.use('/users/:userId/categories/:categoryId/expenses', expenseRouter);
@@ -27,5 +27,5 @@ app.use('/users/:userId/incomes', incomeRouter);
 app.use('/users', userRouter);
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+    console.log(`listening on port ${port}`);
 });
